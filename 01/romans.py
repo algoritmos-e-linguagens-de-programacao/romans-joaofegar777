abd99cd3ad7b614bcde28d3e29a6bd75051ab364
joaofegar777 <168040840+joaofegar777@users.noreply.github.com>
2024-06-05 19:43:49 -0300

    Update romans.py JOÃO FELIPE
    JOÃO FELIPE

--- 01/romans.py
+++ 01/romans.py
@@ -1,8 +1,15 @@
 def int_to_roman(num):
-    # Implemente sua função aqui
-    pass
+    val =[1000,900,500,400,100,90,50,40,10,9,5,4,1]
+    syb =["M","CM","D","CD","C","XC","L","XL","L","Xl","X","IX","V","IV","I"]
+    
+    roman_num =''
+    i = 0
 
-
-def roman_to_int(s):
-    # Implemente sua função aqui
-    pass
+    while num > 0:
+        for _ in range(num // val [i] ):
+            roman_num += syb[i] 
+            num -= val[i]
+            i += 1
+    return roman_num  
+variavel = int_to_roman(1994)
+#print(variavel)       
