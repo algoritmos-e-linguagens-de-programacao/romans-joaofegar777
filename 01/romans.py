13350b169986bc3cbd6962b34190a36074ad29af
joaofegar777 <168040840+joaofegar777@users.noreply.github.com>
2024-06-18 19:35:42 -0300

    Update romans.py
    joao felipe

--- 01/romans.py
+++ 01/romans.py
@@ -1,15 +1,26 @@
 def int_to_roman(num):
-    val =[1000,900,500,400,100,90,50,40,10,9,5,4,1]
-    syb =["M","CM","D","CD","C","XC","L","XL","L","Xl","X","IX","V","IV","I"]
     
-    roman_num =''
+    milhar = ["", "M", "MM", "MMM"]
+    centena = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+    dezena = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+    unidade = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+
+    return milhar[num // 1000] + centena[(num % 1000) // 100] + dezena[(num % 100) // 10] + unidade[num % 10]
+
+def roman_to_int(s):
+    
+    roman_int = { 'I': 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000 }
+    total = 0
     i = 0
 
-    while num > 0:
-        for _ in range(num // val [i] ):
-            roman_num += syb[i] 
-            num -= val[i]
-            i += 1
-    return roman_num  
-variavel = int_to_roman(1994)
-#print(variavel)       
+    for i in range( len(s) ):
+        if i + 1 < len(s) and roman_int[s[i]] < roman_int[s[i + 1]]:
+            total -= roman_int[s[i]]
+
+        else:
+            total += roman_int[s[i]]
+
+    return total
+
+print( int_to_roman(2002) )
+print( roman_to_int('MMII') )
